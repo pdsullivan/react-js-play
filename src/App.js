badd6a1e1f61ec3b73fe21623e97d749bf50b1fe
Patrick Sullivan <patrick@pdsullivan.com>
2016-11-12 18:12:20 -0800

    playing

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+        this.theDate = new Date().toLocaleTimeString();
+    }
+    handleClick() {
+        this.theDate = new Date().toLocaleTimeString();
+        console.log(`date: ${this.theDate}`);
+        //well my ideas dont work haha
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Hellooooo</h2>
+
+                </div>
+                <p className="App-intro" onClick={this.handleClick}>
+                    It is {this.theDate}.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
